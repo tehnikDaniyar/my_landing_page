f924b416cbddd3d499140e7e380a73fd08fd0125
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-05-23 23:01:05 +0600

    started  to create prices calc

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,11 @@ import { observerForHeader } from "./modules/observerForHeader.js";
 import { sliderTop } from "./modules/sliderTop.js";
 import { iconMenu } from "./modules/iconMenu.js";
 import { observerForAnimation } from "./modules/observerForAnimation.js";
+import { pricesCalc } from "./modules/pricesCalc.js";
 
 flsFunctions.isWebp();
 observerForHeader();
 sliderTop();
 iconMenu();
 observerForAnimation();
+pricesCalc();
