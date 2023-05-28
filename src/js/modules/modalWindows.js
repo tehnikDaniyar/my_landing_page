6303920320c1b26f302965dc5f9f59c167df2a11
Daniyar Bashirov <tehnikdaniyar@gmail.com>
2023-05-28 21:12:17 +0600

    ended calc as modal windows

---
+++ src/js/modules/modalWindows.js
@@ -0,0 +1,16 @@
+export const modalWindows = () => {
+	const modalWindows = document.querySelectorAll('.categoryes__item');
+	const selector = document.querySelector('#selector')
+	modalWindows.forEach(modalWindow => {
+		modalWindow.addEventListener('click', (e) => {
+			const target = e.target;
+
+			//=====close button========
+			if (target.classList.contains('categoryes__closeButton') || !target.closest('.categoryes__wrapper')) {
+				target.closest('.categoryes__item').classList.remove('_active');
+				selector.value = 'start';
+				console.log(selector.value);
+			};
+		});
+	});
+}
